838abf6604fccc943b1ded1312359917805e5d49
Mark <mark@remarkablemark.org>
2022-06-06 23:49:47 -0400

    refactor(utils): tidy error.js

--- utils/error.js
+++ utils/error.js
@@ -1,6 +1,9 @@
-module.exports = async (app, ec, utils, err, msgText) => {
-  console.error(err);
+/**
+ * Posts error message.
+ */
+module.exports = async function handleError(app, ec, utils, error, msgText) {
+  console.error(error);
   await app.client.chat.postMessage(
-    utils.msgConfig(ec.botToken, ec.channelID, msgText.error(err))
+    utils.msgConfig(ec.botToken, ec.channelID, msgText.error(error))
   );
 };
