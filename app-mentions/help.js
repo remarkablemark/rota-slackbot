e1ec88db8c9a7a5d02425e3a2b7ebade0fdebe5a
Mark <mark@remarkablemark.org>
2022-06-07 01:08:30 -0400

    refactor(app-mentions): tidy help.js

--- app-mentions/help.js
+++ app-mentions/help.js
@@ -1,16 +1,27 @@
-/*------------------
-  HELP
-  @rota help
-  Provides instructions on how to use Rota
-------------------*/
-module.exports = async (app, ec, utils, helpBlocks, msgText, errHandler) => {
+/**
+ * HELP
+ *
+ * ```
+ * @rota help
+ * ```
+ *
+ * Provides instructions on how to use Rota
+ */
+module.exports = async function helpRotation(
+  app,
+  ec,
+  utils,
+  helpBlocks,
+  msgText,
+  errHandler
+) {
   try {
     await app.client.chat.postMessage({
       token: ec.botToken,
       channel: ec.channelID,
       blocks: helpBlocks(),
     });
-  } catch (err) {
-    errHandler(app, ec, utils, err, msgText);
+  } catch (error) {
+    errHandler(app, ec, utils, error, msgText);
   }
 };
