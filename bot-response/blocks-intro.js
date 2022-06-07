73a16dc936c8bcdb53be345975c05832bbc46c8b
Mark <mark@remarkablemark.org>
2022-06-07 00:31:36 -0400

    style(bot-response): tidy blocks-intro.js

--- bot-response/blocks-intro.js
+++ bot-response/blocks-intro.js
@@ -1,6 +1,6 @@
-/*------------------
-   BLOCKS: INTRO
-------------------*/
+/**
+ * BLOCKS: INTRO
+ */
 const introBlocks = [
   {
     type: 'section',
@@ -9,6 +9,7 @@ const introBlocks = [
       text: ":wave: Hi there! I'm your friendly *Rotation Bot* :arrows_counterclockwise::robot_face: My role is to help track many different rotations across teams.",
     },
   },
+
   {
     type: 'section',
     text: {
@@ -16,6 +17,7 @@ const introBlocks = [
       text: ':speech_balloon: *If you need help with a rotation, post a message to any channel I\'ve been added to and mention `@rota "[rotation-name]" [your message]`*. The person on duty for the rotation will be notified!',
     },
   },
+
   {
     type: 'divider',
   },
